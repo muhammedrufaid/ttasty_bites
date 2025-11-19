7bbfb71f50efc083e1e776a63b1efd6e170bc600
muhammed rufaid <muhammedrufaid@muhammeds-MacBook-Air.local>
2025-11-20 00:27:19 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-4">Hello Tailwind!</div>
+      <div className="bg-green-500 text-white p-4">Another Tailwind component!</div>
+      <div className="bg-red-500 text-white p-4">Yet another Tailwind component!</div>
+    </>
+  );
+}
+
+export default App;
